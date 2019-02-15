ba9468930b20d8c2b13ce7e1ed7d328341250945
Tyler Zhu <tylerz@nvidia.com>
2019-02-15 16:49:12 +0800

    Change UNet from NHWC to NCHW

--- segmentation_models/unet/blocks.py
+++ segmentation_models/unet/blocks.py
@@ -4,6 +4,7 @@ from keras.layers import Conv2D
 from keras.layers import BatchNormalization
 from keras.layers import Activation
 from keras.layers import Concatenate
+import keras.backend as K
 
 
 def handle_block_names(stage):
@@ -18,7 +19,7 @@ def ConvRelu(filters, kernel_size, use_batchnorm=False, conv_name='conv', bn_nam
     def layer(x):
         x = Conv2D(filters, kernel_size, padding="same", name=conv_name, use_bias=not(use_batchnorm))(x)
         if use_batchnorm:
-            x = BatchNormalization(name=bn_name)(x)
+            x = BatchNormalization(name=bn_name, axis=1)(x)
         x = Activation('relu', name=relu_name)(x)
         return x
     return layer
@@ -34,7 +35,7 @@ def Upsample2D_block(filters, stage, kernel_size=(3,3), upsample_rate=(2,2),
         x = UpSampling2D(size=upsample_rate, name=up_name)(input_tensor)
 
         if skip is not None:
-            x = Concatenate()([x, skip])
+            x = Concatenate(axis=1)([x, skip])
 
         x = ConvRelu(filters, kernel_size, use_batchnorm=use_batchnorm,
                      conv_name=conv_name + '1', bn_name=bn_name + '1', relu_name=relu_name + '1')(x)
@@ -55,12 +56,13 @@ def Transpose2D_block(filters, stage, kernel_size=(3,3), upsample_rate=(2,2),
 
         x = Conv2DTranspose(filters, transpose_kernel_size, strides=upsample_rate,
                             padding='same', name=up_name, use_bias=not(use_batchnorm))(input_tensor)
+
         if use_batchnorm:
-            x = BatchNormalization(name=bn_name+'1')(x)
+            x = BatchNormalization(name=bn_name+'1', axis=1)(x)
         x = Activation('relu', name=relu_name+'1')(x)
 
         if skip is not None:
-            x = Concatenate()([x, skip])
+            x = Concatenate(axis=1)([x, skip])
 
         x = ConvRelu(filters, kernel_size, use_batchnorm=use_batchnorm,
                      conv_name=conv_name + '2', bn_name=bn_name + '2', relu_name=relu_name + '2')(x)
